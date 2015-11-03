fb2542f0a76591f7c4b92a740090dacc4654ed18
Xuekai Niu <xkniu@users.noreply.github.com>
2015-11-03 11:46:28 +0800

    Test sorting with the same unsorted data

--- src/test/java/org/algorithmprac/sort/SortTester.java
+++ src/test/java/org/algorithmprac/sort/SortTester.java
@@ -9,28 +9,34 @@ public class SortTester {
 
     private static final Logger log = LoggerFactory.getLogger(SortTester.class);
 
+    private static Integer[] testCaseData;
+
+    static {
+        testCaseData = DataProvider.getUnorderedIntegerArray(100, 10000);
+    }
+
+    private static Integer[] getTestCaseDataCopy() {
+        return testCaseData.clone();
+    }
+
     @Test
     public void testSelectionSort() {
         CostAwareSorter costAwareSorter = Sorters.getSorter(Sorters.SorterType.SELECTION);
-        costAwareSorter.sort(prepareData());
+        costAwareSorter.sort(getTestCaseDataCopy());
         log.info("{}, {}", costAwareSorter.getName(), costAwareSorter.getReadableCost());
     }
 
     @Test
     public void testInsertionSort() {
         CostAwareSorter costAwareSorter = Sorters.getSorter(Sorters.SorterType.INSERTION);
-        costAwareSorter.sort(prepareData());
+        costAwareSorter.sort(getTestCaseDataCopy());
         log.info("{}, {}", costAwareSorter.getName(), costAwareSorter.getReadableCost());
     }
 
     @Test
     public void testBubbleSort() {
         CostAwareSorter costAwareSorter = Sorters.getSorter(Sorters.SorterType.BUBBLE);
-        costAwareSorter.sort(prepareData());
+        costAwareSorter.sort(getTestCaseDataCopy());
         log.info("{}, {}", costAwareSorter.getName(), costAwareSorter.getReadableCost());
-    }
-
-    private Integer[] prepareData() {
-        return DataProvider.getUnorderedIntegerArray(500, 10000);
     }
 }
